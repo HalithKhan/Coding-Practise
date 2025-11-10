27291cab415fdb44c20ffa8a9f99f99be7cda3ea
HalithKhan <halithak73@gmail.com>
2025-11-10 23:02:15 +0530

    Add Factorial class implementation

---
+++ java-problems/Factorial.java
@@ -0,0 +1,19 @@
+public class Factorial {
+    // Recursive factorial
+    public int recursiveFactorial(int n) {
+        // TODO: Implement recursive factorial
+        return -1;
+    }
+
+    // Iterative factorial
+    public int iterativeFactorial(int n) {
+        // TODO: Implement iterative factorial
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println(f.recursiveFactorial(5)); // 120
+        System.out.println(f.iterativeFactorial(5)); // 120
+    }
+}
