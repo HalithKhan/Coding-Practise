892538928a04fb45f9ef33904cb7df20a052aa66
HalithKhan <halithak73@gmail.com>
2025-11-10 23:02:04 +0530

    Add FizzBuzz implementation skeleton

---
+++ java-problems/FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+    // Print numbers from 1 to n, with FizzBuzz rules
+    public void printFizzBuzz(int n) {
+        // TODO: Implement FizzBuzz logic
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.printFizzBuzz(15);
+    }
+}
