ac86fa9d39559b6be3c26a667caedfe214e44596
HalithKhan <halithak73@gmail.com>
2025-11-10 23:02:09 +0530

    Add PalindromeNumber.java implementation

---
+++ java-problems/PalindromeNumber.java
@@ -0,0 +1,13 @@
+public class PalindromeNumber {
+    // Returns true if x is a palindrome number
+    public boolean isPalindrome(int x) {
+        // TODO: Implement palindrome check
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber pn = new PalindromeNumber();
+        System.out.println(pn.isPalindrome(121)); // true
+        System.out.println(pn.isPalindrome(-121)); // false
+    }
+}
