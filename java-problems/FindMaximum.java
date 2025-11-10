b701a95c525d125fe72d7bbd0c1c0a7daa86f87f
HalithKhan <halithak73@gmail.com>
2025-11-10 23:02:21 +0530

    Add FindMaximum.java to find the maximum number from an array

---
+++ java-problems/FindMaximum.java
@@ -0,0 +1,12 @@
+public class FindMaximum {
+    // Returns the largest number in the array
+    public int findMax(int[] nums) {
+        // TODO: Implement logic to find max
+        return Integer.MIN_VALUE;
+    }
+
+    public static void main(String[] args) {
+        FindMaximum fm = new FindMaximum();
+        System.out.println(fm.findMax(new int[]{1, 3, 2, 7, 5})); // 7
+    }
+}
