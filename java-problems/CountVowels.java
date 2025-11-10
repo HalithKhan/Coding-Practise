094eafa9a78833645bd4893b797f96d7a0e10025
HalithKhan <halithak73@gmail.com>
2025-11-10 23:02:28 +0530

    Add CountVowels.java with vowel counting implementation

---
+++ java-problems/CountVowels.java
@@ -0,0 +1,12 @@
+public class CountVowels {
+    // Counts number of vowels in a string
+    public int countVowels(String s) {
+        // TODO: Implement vowel counting
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        CountVowels cv = new CountVowels();
+        System.out.println(cv.countVowels("OpenAI")); // 3
+    }
+}
