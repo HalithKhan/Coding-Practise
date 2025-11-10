3abaa04b9ea468b81d277da8c6ee09a156c3af0a
HalithKhan <halithak73@gmail.com>
2025-11-10 23:01:53 +0530

    Add ReverseString.java implementation

---
+++ java-problems/ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    // Function to reverse a String
+    public String reverse(String s) {
+        // TODO: Implement logic to reverse string
+        return "";
+    }
+
+    public static void main(String[] args) {
+        ReverseString rs = new ReverseString();
+        System.out.println(rs.reverse("hello")); // Output: olleh
+    }
+}
